34f29435ea7fbf150a415f1b3719fa7cd46fb37a
oscaroceguerabibriesca <oscaroceguera@gmail.com>
2016-12-23 00:09:47 -0700

    Koa con vistas

--- index.js
+++ index.js
@@ -1,72 +1,24 @@
 var koa = require('koa')
 var router = require('koa-router')
-var mongoose = require('mongoose')
-var parse = require('co-body')
+var views = require('co-views')
 
-// define the user schema
-var userSchema = new mongoose.Schema({
-  'firstname': {
-    type: String,
-    required: true
-  },
-  'lastname': {
-    type: String,
-    required: true
-  }
-})
-
-// turn schema into a mongoose model
-var User = mongoose.model('User', userSchema)
+var render = views(__dirname + '/views',  { ext: 'ejs'})
 
 var app = koa()
 
 app.use(router(app))
 
-app.use(function* (next) {
-  // before
-  var timestampBefore = new Date().getTime()
-  yield next
+app.get('/', index)
+app.get('/about', about)
 
-  // after
-  var timestampAfter = new Date().getTime()
-  var difference = timestampAfter - timestampBefore
-  console.log('Process took ' + difference + 'ms');
+function *index() {
+  this.body = yield render('index', {})
+}
 
-})
-
-app.use(function* () {
-  this.body = 'hello word!'
-})
-
-app.all('/users', function* (next) {
-  switch (this.request.method) {
-    case 'GET':
-      try {
-        var users = []
-        this.body = users
-      } catch(e) {
-        this.trhow(500, e)
-      }
-      break;
-    case 'POST':
-      // parse the request body
-      var body = yield parse(this)
-
-      // create the user which we want to save
-      var userToSave = new User({
-        firstname: body.firstname,
-        lastname: body.lastname
-      })
-
-      // try to save it, catch errors
-      try {
-        yield userToSave.save()
-        this.body = 'Success!'
-      } catch(e) {
-        this.throw(500, e)
-      }
-      break
-  }
-})
+function *about() {
+  console.log('Koa Context (this) has these properties: ');
+  console.log(Object.keys(this));
+  this.body = "<h2>This is the about route</h2>";
+}
 
 app.listen(3000)
